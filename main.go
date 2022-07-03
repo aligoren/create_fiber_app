b33c88778b65d6fbd464b835a0d964e2eb174152
Ali GOREN <goren.ali@yandex.com>
2022-07-03 16:13:45 +0300

    added comment

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"create_fiber_app/commands"
-	cli "github.com/urfave/cli/v2"
 	"log"
 	"os"
+
+	cli "github.com/urfave/cli/v2"
 )
 
 func main() {
+
+	// These settings can be changed
 	app := &cli.App{
 		Name:        "Create Fiber App",
 		Usage:       "Helps your Fiber development easy",
