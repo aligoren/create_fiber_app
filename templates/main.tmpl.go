6580d88d011ab3818f303e2bb00eaa5f03de04cc
Ali GOREN <goren.ali@yandex.com>
2022-07-03 16:41:30 +0300

    example file generator started

---
+++ templates/main.tmpl.go
@@ -0,0 +1,17 @@
+package templates
+
+var MAIN_TEMPLATE string = `
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":3000")
+}
+`
